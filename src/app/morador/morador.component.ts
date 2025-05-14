ae4681304d1ea2300a9235bd8eb5b7027c2f5d4a
Michelle Madeira <michellemadeira1@gmail.com>
2025-05-14 11:45:06 -0300

    Start-Project

---
+++ src/app/morador/morador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-morador',
+  imports: [],
+  templateUrl: './morador.component.html',
+  styleUrl: './morador.component.scss'
+})
+export class MoradorComponent {
+
+}
