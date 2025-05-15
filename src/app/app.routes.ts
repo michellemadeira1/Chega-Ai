dbc4de4d9c4b584824637e76b63ab68c2fafbfc2
Michelle Madeira <michellemadeira1@gmail.com>
2025-05-15 17:03:20 -0300

    Pagina - Cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from "./login/login.component";
 import { MoradorComponent } from "./morador/morador.component";
 import { MainLoginComponent } from "./main-login/main-login.component";
 import { EncomendasComponent } from "./encomendas/encomendas.component";
+import { CadastroComponent } from "./cadastro/cadastro.component";
 
 export const routes: Routes = [
    { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -13,5 +14,6 @@ export const routes: Routes = [
   { path: 'mainLogin',  component: MainLoginComponent },
   { path: 'morador', component: MoradorComponent },
   { path: 'entregas', component: EntregasComponent },
-  { path: 'encomenda', component:  EncomendasComponent }
+  { path: 'encomenda', component:  EncomendasComponent },
+   { path: 'cadastro', component:  CadastroComponent  }
 ];
---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss'
+})
+export class CadastroComponent {
+
+}
