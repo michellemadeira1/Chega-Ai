d04299d10290731545322399e09414ab2f10daec
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-05-18 05:17:36 -0300

    import about component

--- src/app/landing/index/index.component.ts
+++ src/app/landing/index/index.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../header/header.component';
 import { HeroComponent } from '../hero/hero.component';
 import { BenefitsComponent } from "../benefits/benefits.component";
 import { HowworksComponent } from '../howworks/howworks.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, BenefitsComponent, HowworksComponent],
+  imports: [HeaderComponent, HeroComponent, BenefitsComponent, HowworksComponent, AboutComponent],
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
