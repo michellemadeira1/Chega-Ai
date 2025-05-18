ce377926306c886385642e356484714370aad1eb
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-05-18 04:57:17 -0300

    import benefits component

--- src/app/landing/index/index.component.ts
+++ src/app/landing/index/index.component.ts
@@ -1,11 +1,12 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { HeroComponent } from '../hero/hero.component';
+import { BenefitsComponent } from "../benefits/benefits.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeaderComponent, HeroComponent, BenefitsComponent],
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
