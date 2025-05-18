fb5f1ed05c2ca89c2cd36905bc1608ac2001a36b
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-05-18 04:51:24 -0300

    importando o component Hero

--- src/app/landing/index/index.component.ts
+++ src/app/landing/index/index.component.ts
@@ -1,10 +1,11 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { HeroComponent } from '../hero/hero.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HeroComponent],
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
