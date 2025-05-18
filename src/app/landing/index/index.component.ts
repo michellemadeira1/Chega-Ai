c55409c6c29ab2c9aa5631f1d1d96994dc521694
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-05-18 05:29:38 -0300

    insert contact component

--- src/app/landing/index/index.component.ts
+++ src/app/landing/index/index.component.ts
@@ -4,11 +4,12 @@ import { HeroComponent } from '../hero/hero.component';
 import { BenefitsComponent } from "../benefits/benefits.component";
 import { HowworksComponent } from '../howworks/howworks.component';
 import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, BenefitsComponent, HowworksComponent, AboutComponent],
+  imports: [HeaderComponent, HeroComponent, BenefitsComponent, HowworksComponent, AboutComponent, ContactComponent],
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
